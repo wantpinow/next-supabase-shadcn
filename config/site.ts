45f9ba1f7bf57cb3732077bd8690ce755972a1dc
Patrick Frenett <patrick@frenett.net>
2023-11-25 11:22:22 +0000

    feat: clean up config

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+// the metadata for the whole site, can be overridden by page metadata
+export const defaultMetadata: Metadata = {
+  title: "Next.js, Supabase, shadcn/ui",
+  description: "A Next.js template with Supabase and shadcn/ui",
+};
